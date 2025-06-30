e803c2ed74e6eead8e7f67fdf4dd17ba2fa21131
Brandon K. Waltz <bwaltz6@gmail.com>
2025-06-30 14:20:57 -0400

    Update giveButton.cy.js

--- project/site_specific/tests/cypress/giveButton.cy.js
+++ project/site_specific/tests/cypress/giveButton.cy.js
@@ -1,7 +1,7 @@
 describe('Give Button', () => {
     it('Should go to ways to give page', () => {
         cy.visit('/');
-        cy.get('.header__utility > ul > li:first-child a').click();
+        cy.get('.header__utility > ul > li:first-child a').click({ force: true });
         cy.location('pathname', { timeout: 3000 }).should('include', '/ways-to-give/give/');
     });
 });
