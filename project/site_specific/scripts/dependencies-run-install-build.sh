b5aedcd488bec54e36b1744caefd92f9ced0cf47
Brandon K. Waltz <bwaltz6@gmail.com>
2025-06-30 13:32:37 -0400

    maintenance

---
+++ project/site_specific/scripts/dependencies-run-install-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+printf "Installing NPM dependencies for Colby dependencies \n"
+
+shopt -s extglob # Turns on extended globbing
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+printf "Build Colby Theme... \n"
+cd web/wp-content/themes/colby-college-theme
+composer install
+composer dump-autoload
+yarn
+yarn scripts:build
+cd -
+
+printf "Build Advancement Theme... \n"
+cd web/wp-content/themes/colby-advancement-theme
+composer install
+composer dump-autoload
+yarn
+yarn scripts:build
+cd -
+
+
+# npm install
+shopt -u extglob
