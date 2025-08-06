d5f985ee1b0b9e52abf69cc80c858251e2c0e253
Brandon K. Waltz <bwaltz6@gmail.com>
2025-08-06 13:05:44 -0400

    Create createNewEvent.cy.js

---
+++ project/site_specific/tests/cypress/createNewEvent.cy.js
@@ -0,0 +1,48 @@
+describe('Event Creation', () => {
+    before(() => {
+        // Login via UI and set cookies
+        cy.visit('/wp/wp-admin/');
+        cy.get('#user_login').type(Cypress.env('WP_USERNAME'));
+        cy.get('#user_pass').type(Cypress.env('WP_PASSWORD'));
+        cy.get('#wp-submit').click();
+    });
+
+    it('Creates a new page', () => {
+        cy.visit('/wp/wp-admin/post-new.php?post_type=events');
+        cy.get('.editor-post-title__input').type('My New Event');
+        cy.get('#acf-field_646157c578254').type('August 6, 1970 1:00 pm');
+        cy.get('#acf-field_6679a58545b3e').type('August 6, 1970 2:00 pm');
+        cy.get('.editor-post-publish-button__button').click();
+        cy.wait(2000);
+
+        cy.get(
+            '.interface-interface-skeleton__actions .editor-post-publish-button__button'
+        ).click();
+        cy.wait(1000);
+
+        // Verify page is published
+        cy.get('.components-snackbar__content').should('contain', 'Post published.');
+    });
+});
+
+describe('New Event', () => {
+    it('Should match the new event', () => {
+        cy.visit('events/event-calendar/fdsafdsa/');
+        cy.get('.text-group__heading > span').should('equal', 'My New Event');
+        cy.get('.text-group__subheading > span').should('have.length', 5);
+    });
+});
+
+describe('New Event Calendar', () => {
+    it('Should match the new event', () => {
+        cy.visit('events/past-events/');
+        cy.get('article-grid grid > .article-grid__item:last-child h2').should(
+            'equal',
+            'My New Event'
+        );
+        cy.get('article-grid grid > .article-grid__item:last-child .text-group__subheading').should(
+            'have.length',
+            11
+        );
+    });
+});
