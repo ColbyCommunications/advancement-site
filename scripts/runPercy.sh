02fc686975c38763b0d41c954ce6607b45bdb651
Brandon K. Waltz <bwaltz6@gmail.com>
2025-08-08 13:01:22 -0400

    Update runPercy.sh

--- scripts/runPercy.sh
+++ scripts/runPercy.sh
@@ -5,10 +5,10 @@ export PERCY_PARALLEL_NONCE=$(uuidgen)
 export PERCY_PARALLEL_TOTAL=2
 
 # Run the first snapshot script in a separate process
-percy exec --parallel --total=$PERCY_PARALLEL_TOTAL --nonce=$PERCY_PARALLEL_NONCE -- node project/site_specific/tests/percy/percy.js &
+npx percy exec --parallel --total=$PERCY_PARALLEL_TOTAL --nonce=$PERCY_PARALLEL_NONCE -- node project/site_specific/tests/percy/percy.js &
 
 # Run the second snapshot script in a separate process
-percy exec --parallel --total=$PERCY_PARALLEL_TOTAL --nonce=$PERCY_PARALLEL_NONCE -- node project/global/tests/percy/percy.js &
+npx percy exec --parallel --total=$PERCY_PARALLEL_TOTAL --nonce=$PERCY_PARALLEL_NONCE -- node project/global/tests/percy/percy.js &
 
 # Wait for both background processes to finish
 wait
