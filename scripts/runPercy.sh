36a218d84fefcd1a4ef10fc655b10f21dd210af8
Brandon K. Waltz <bwaltz6@gmail.com>
2025-08-08 12:21:21 -0400

    updates

---
+++ scripts/runPercy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# A unique ID to group these two jobs into a single build
+export PERCY_PARALLEL_NONCE=$(uuidgen)
+export PERCY_PARALLEL_TOTAL=2
+
+# Run the first snapshot script in a separate process
+npx percy exec --parallel --total=$PERCY_PARALLEL_TOTAL --nonce=$PERCY_PARALLEL_NONCE -- node project/site_specific/tests/percy/percy.js &
+
+# Run the second snapshot script in a separate process
+percy exec --parallel --total=$PERCY_PARALLEL_TOTAL --nonce=$PERCY_PARALLEL_NONCE -- node project/global/tests/percy/percy.js &
+
+# Wait for both background processes to finish
+wait
